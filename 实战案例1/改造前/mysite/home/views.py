e639cb82ac3e8dda0b93d814518551d2a57d160e
Weimin Ye <weiminye@users.noreply.github.com>
2024-07-06 15:36:55 +0800

    完成新增记录

--- 实战案例1/改造前/mysite/home/views.py
+++ 实战案例1/改造前/mysite/home/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from .models import 销售入账记录
 
 def index(request):
     # return HttpResponse("home index")
@@ -7,8 +10,23 @@ def index(request):
 
 def salescheck(request):
     # return HttpResponse("home index")
-    return render(request, "home/salescheck.html")
+    if request.method == 'POST':
+        print(request.POST)
+    else:
+        object_list = 销售入账记录.objects.all()
+
+    return render(request, "home/salescheck.html",context={"object_list":object_list})
 
 def addsalescheck(request):
-    # return HttpResponse("home index")
-    return render(request, "home/addsalescheck.html")
+    if request.method == 'POST':
+        record = 销售入账记录()
+        record.客户 = request.POST['name']
+        record.入账日期 = request.POST['created_at']
+        record.入账金额 = request.POST['amount']
+        record.已到账款项 = request.POST['total']
+        record.剩余到账款项 = request.POST['leave']
+
+        record.save()
+        return redirect(reverse('home:salescheck'))
+    else:
+        return render(request, "home/addsalescheck.html")
