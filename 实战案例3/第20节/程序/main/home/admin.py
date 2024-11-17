7a762a75721ccdc89c797134fc1ef8ab72b6716b
Billy Yeah <billyinusa@outlook.com>
2024-11-17 12:17:00 +0800

    fix admin导出excel bug

--- 实战案例3/第20节/程序/main/home/admin.py
+++ 实战案例3/第20节/程序/main/home/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 from .models import 对话记录
-from import_export.admin import ImportExportModelAdmin, ExportActionMixin
+from import_export.admin import ImportExportModelAdmin
 
-class 对话记录Admin(ImportExportModelAdmin, ExportActionMixin):
+class 对话记录Admin(ImportExportModelAdmin):
     list_display = ['不带入大模型对话中','role', '处理后content','positive_review','negative_review']
     search_fields = ['positive_review','negative_review']
     list_filter = ['positive_review','negative_review']
