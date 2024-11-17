54054aeaf267a712fd3d7cb1bd33ae42b38b4f24
Billy Yeah <billyinusa@outlook.com>
2024-11-17 10:59:51 +0800

    完成好评和差评按钮

--- 实战案例3/第20节/程序/main/home/admin.py
+++ 实战案例3/第20节/程序/main/home/admin.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 from .models import 对话记录
 
 class 对话记录Admin(admin.ModelAdmin):
-    ordering = ["created_time"]
-    list_display = ['已结束','created_time','不带入大模型对话中','role', 'content', '处理后content','提交给大模型的playload']
-    search_fields = ['已结束']
-    list_filter = ('已结束',)
+    list_display = ['不带入大模型对话中','role', '处理后content','positive_review','negative_review']
+    search_fields = ['positive_review','negative_review']
+    list_filter = ['positive_review','negative_review']
 
 admin.site.register(对话记录, 对话记录Admin)
