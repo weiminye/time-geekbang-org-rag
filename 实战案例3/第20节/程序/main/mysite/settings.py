7718a402e873fd4a822bca9c0c507b3d6b7172ca
Billy Yeah <billyinusa@outlook.com>
2024-11-25 16:36:04 +0800

    add INSTALLED_APPS = [     'import_export',

--- 实战案例3/第20节/程序/main/mysite/settings.py
+++ 实战案例3/第20节/程序/main/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
